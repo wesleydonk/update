74849618496fd762a7875fd7d42ad11b22ed9451
wesley <wesleydonk@gmail.com>
2021-07-12 23:05:05 +0200

    update-ui-no-op implementation

---
+++ update-ui-no-op/src/main/java/com/wesleydonk/update/ui/internal/extensions/ContextExtensions.kt
@@ -0,0 +1,6 @@
+package com.wesleydonk.update.ui.internal.extensions
+
+import androidx.fragment.app.FragmentActivity
+import com.wesleydonk.update.Version
+
+fun Version.showTryFragment(activity: FragmentActivity) = Unit
