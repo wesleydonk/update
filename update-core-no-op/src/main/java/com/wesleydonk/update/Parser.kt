6ef083d1cf16ead64bfef0b43de2d840cb3f1a85
wesley <wesleydonk@gmail.com>
2021-11-15 08:21:19 +0100

    Deprecated parser now mapping is without the metadata

--- update-core-no-op/src/main/java/com/wesleydonk/update/Parser.kt
+++ update-core-no-op/src/main/java/com/wesleydonk/update/Parser.kt
@@ -1,9 +1,11 @@
 package com.wesleydonk.update
 
+@Deprecated("Unnecessary mapping which can be done in a fetcher instance")
 interface Parser {
     fun parse(version: CheckVersionResult): Version
 }
 
+@Deprecated("Unnecessary mapping which can be done in a fetcher instance")
 class DefaultParser : Parser {
     override fun parse(version: CheckVersionResult): Version {
         return Version(
